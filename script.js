a96491c400b47966d152e1a6e632c38a1b604694
Elisabetta <156212818+EliMarago@users.noreply.github.com>
2025-05-29 19:26:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
+const resultsEl = document.querySelector(".result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
-  number: getRandoNumber,
+  number: getRandomNumber,
   symbol: getRandomsymbol,
 };
+
+//check checkbox
+generateEl.addEventListener("click", function () {
+  const length = Number(lengthEl.value); // prende il valore numerico
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultsEl.innerText = generetePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+function generetePassword(lower, upper, number, symbol, length) {
+  let generetedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  if (typesCount === 0) {
+    return "";
+  }
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generetedPassword += randomFunc[funcName]();
+    });
+  }
+  return generetedPassword.slice(0, length);
+}
 
 //fromCharCode - Ogni numero in Unicode (o ASCII per i caratteri base) corrisponde a un simbolo o un carattere
 function getRandomLower() {
@@ -13,7 +56,7 @@ function getRandomLower() {
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-function getRandoNumber() {
+function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 function getRandomsymbol() {
